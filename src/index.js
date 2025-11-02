82855762289f07f88491f5fbc0825944f625a6cc
Okpala Emmanuel <merison200@gmail.com>
2025-11-02 22:13:59 +0100

    feat: prepare for Render deployment - add start script and update CORS

--- src/index.js
+++ src/index.js
@@ -30,7 +30,11 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
+    origin: [
+      process.env.CLIENT_URL, 
+      process.env.FRONTEND_URL,
+      "http://localhost:5173"
+    ],
     credentials: true,
   })
 );
